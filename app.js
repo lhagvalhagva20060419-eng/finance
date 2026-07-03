bcbe74bdaf2d4070d8806bd08d3e286820e89668
lhagva <lhagvalhagva20060419@gmail.com>
2026-07-03 17:16:14 +0800

    finance module hiij ehelsen

--- app.js
+++ app.js
@@ -1,26 +1,74 @@
 // delgetstei ajillah controller
-var uiController = (function () {})();
+var uiController = (function () {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add_value",
+    inputBtn: ".add__btn",
+  };
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+  };
+})();
 
 // sanhuutei ajillah controller
-var financeController = (function () {})();
+var financeController = (function () {
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      inc: [],
+      exp: [],
+    },
+
+    totals: {
+      inc: 0,
+      exp: 0,
+    },
+  };
+})();
 
 // programmiig holboh controller
 var appController = (function (uiController, financeController) {
-  document.querySelector(".add__btn").addEventListener("click", function () {
-    // oruulah ogogdliig delgetsees awah
-    // ogogdloo sanhuutei controller ruu damjuulah
-    // delgetsend uzuuleh
-    // tosviig shinechleh
-    // etssiin uldegdel tootsoog delgetsend uzuuleh
-  });
+  var DOM = uiController.getDOMstrings();
+  var ctrlAddItem = function () {};
 
-  document.addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-      // oruulah ogogdliig delgetsees awah
-      // ogogdloo sanhuutei controller ruu damjuulah
-      // delgetsend uzuuleh
-      // tosviig shinechleh
-      // etssiin uldegdel tootsoog delgetsend uzuuleh
-    }
-  });
+  var setupEventListeners = function () {
+    document
+      .querySelector(DOM.inputBtn)
+      .addEventListener("click", function () {});
+
+    document.addEventListener("keypress", function (event) {
+      if (event.key === "Enter") {
+      }
+    });
+  };
+
+  return {
+    init: function () {
+      console.log("Application has started.");
+      setupEventListeners();
+    },
+  };
 })(uiController, financeController);
+
+appController.init();
